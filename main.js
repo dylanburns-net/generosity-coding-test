84426ea4ea9df1801bfb5953b61ab0220cc30de6
dylanburns-net <55495398+dylanburns-net@users.noreply.github.com>
2024-02-09 14:05:04 +0900

    もっと見やすいように背景色を変更

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( '#dddddd' );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
